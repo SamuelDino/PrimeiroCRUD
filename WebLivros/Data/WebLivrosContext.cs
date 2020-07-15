fc467f27be9c09b36a0c827fe3704a8929ee525e
Samuel <samuel_qualitats@hotmail.com>
2020-07-15 00:36:45 -0300

    Primeiro GRUD

---
+++ WebLivros/Data/WebLivrosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebLivros.Models;
+
+namespace WebLivros.Data
+{
+    public class WebLivrosContext : DbContext
+    {
+        public WebLivrosContext (DbContextOptions<WebLivrosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebLivros.Models.Livro> Livro { get; set; }
+    }
+}
